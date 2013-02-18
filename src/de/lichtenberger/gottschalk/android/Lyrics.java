efb53bbfbf05e87b1a375f2a73982c5eba884a8f
Paul <paul32@gmx.net>
2013-02-18 16:19:08 +0100

    Datenbank holen geht noch nicht

--- src/de/lichtenberger/gottschalk/android/Lyrics.java
+++ src/de/lichtenberger/gottschalk/android/Lyrics.java
@@ -5,14 +5,11 @@ import java.util.List;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.database.DataSetObserver;
 import android.os.Bundle;
 import android.view.View;
-import android.view.ViewGroup;
 import android.view.View.OnClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
-import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -25,10 +22,10 @@ import com.parse.ParseQuery;
 public class Lyrics extends Activity {
 
 	ImageButton back;
-	
+	ListView songList;
 	TextView textAnzeige;
-	ArrayList<String> Titelliste;
-	
+	private ArrayList<String> Titelliste;
+	private ArrayAdapter<String> aa;
 	
 		
 	
@@ -38,58 +35,55 @@ public class Lyrics extends Activity {
 	        setContentView(R.layout.activity_lyrics);
 	        
 	        Parse.initialize(this, "hGxasGU6e0WQAOh5JIOGDfvFBKrYyBJKXIzxBfAG", "WsOPsXerpsFjsjekKKbZnnjAHvXy5PQHVQEB8Cqu");
-	        
-	        
-	        
-	        
-	        
-	        
-	        
+	        	        	      
 	        setupUI();
 	        setupOnClickListeners();
-	       
-	        
-	        
-	        ParseQuery pq = new ParseQuery("SongDatenbank");
-	        final ArrayAdapter<String> aa = new ArrayAdapter<String>(Lyrics.this, android.R.layout.simple_list_item_1);
-	        pq.whereExists("SessionID");
-	       
-	        pq.findInBackground(new FindCallback() {
-				
-				@Override
-				public void done(List<ParseObject> objects, ParseException e) {
-					if (e==null){
-						
-						
-						
-						for(int i = 0; i<objects.size();i++){
-							Object object = objects.get(i);
-							String titel = ((ParseObject)object).getString("Titel").toString();
-							aa.add(titel);
-							
-						}
-						
-						 ListView songs = (ListView)findViewById(R.id.lyricsView);
-					     songs.setAdapter(aa);
-					     aa.setNotifyOnChange(true);
-					}
-					else {
-						
-					}
-				}
-			});
-	     
-	        
-	        
-	        
-	        
-	        
-	        
-	        
-	        
-	       
+	       	     	        
+	        getSongs();
+	        initDataToView();  
 	        
 	    }
+
+
+
+
+	private void initDataToView() {
+		aa = new ArrayAdapter<String>(Lyrics.this, android.R.layout.simple_list_item_1, Titelliste);
+		songList = (ListView)findViewById(R.id.SongView);
+		songList.setAdapter(aa);
+	}
+
+
+
+
+	private void getSongs() {
+		ParseQuery pq = new ParseQuery("SongDatenbank");
+		
+		pq.whereExists("SessionID");
+      
+		pq.findInBackground(new FindCallback() {
+			
+			@Override
+			public void done(List<ParseObject> objects, ParseException e) {
+				if (e==null){
+					
+					
+					
+					for(int i = 0; i<objects.size();i++){
+						Object object = objects.get(i);
+						String titel = ((ParseObject)object).getString("Titel").toString();
+						Titelliste.add(titel);
+						
+					}
+					aa.setNotifyOnChange(true);
+					songList.setAdapter(aa);
+				}
+				else {
+					
+				}
+			}
+		});
+	}
 
 	
 	 
@@ -106,17 +100,12 @@ public class Lyrics extends Activity {
 				
 			}
 		});
-		
 	}
 
 	private void setupUI() {
 		
 		back = (ImageButton)findViewById(R.id.back);
 		textAnzeige = (TextView)findViewById(R.id.lyricsView);
-		
-		
-	}
-
-
-	
-	}
+				
+	}	
+}
